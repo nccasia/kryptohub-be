41a27063ec2b258714f0809e6d372eb0ef850bbd
Dai Trinh Duc <daitd@qsoftvietnam.com>
2022-04-29 16:52:47 +0700

    add swagger

--- src/metadiscs/dto/create-metadisc.dto.ts
+++ src/metadiscs/dto/create-metadisc.dto.ts
@@ -1,42 +1,62 @@
 import {IsNotEmpty} from 'class-validator';
+import {ApiProperty} from '@nestjs/swagger';
 
 export class CreateMetadiscDto {
+    @ApiProperty()
     @IsNotEmpty()
     name!: string;
 
+    @ApiProperty()
     @IsNotEmpty()
     uri!: string;
 
+    @ApiProperty()
     @IsNotEmpty()
     status!: string;
 
+    @ApiProperty()
     @IsNotEmpty()
     tokenId!: string;
 
+    @ApiProperty()
     @IsNotEmpty()
     type!: string;
 
+    @ApiProperty()
     @IsNotEmpty()
     version!: string;
 
+    @ApiProperty()
     @IsNotEmpty()
     sub_type!: string;
 
+    @ApiProperty()
     @IsNotEmpty()
     total_tracks!: string;
 
+    @ApiProperty()
     @IsNotEmpty()
     thumbnail!: string;
 
-    // @IsNotEmpty()
+    //@ApiProperty()
+    //@IsNotEmpty()
     //release_date: Date;
-
-    // @IsNotEmpty()
+    //@ApiProperty()
+    //@IsNotEmpty()
     // release_date_precision: string;
 
+    @ApiProperty()
     @IsNotEmpty()
     description!: string;
 
+    @ApiProperty({
+        example: {
+            street: '1555 Blake St',
+            city: 'Denver',
+            county: 'Denver',
+            state: 'CO',
+        },
+    })
     @IsNotEmpty()
     location!: JSON;
 }
