17a342e3924d860a59b728b37bb57260362d6de3
Toan Nguyen <toan.nguyenviet@ncc.asia>
2022-06-01 10:02:14 +0700

    add slogan into migration

--- src/migrations/1653973001413-create-teams.ts
+++ src/migrations/1653973001413-create-teams.ts
@@ -5,7 +5,7 @@ export class createTeams1653973001413 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(
-            `CREATE TABLE "team" ("id" SERIAL NOT NULL, "teamName" character varying NULL, "teamSize" character varying NULL, "timeZone" character varying NULL, "organization" character varying NULL, "workingTime" character varying NULL, "hour" character varying NULL, "week" character varying NULL, "description" character varying NULL, "avatar" character varying NULL, "avatarUrl" character varying NULL, "founded" character varying NULL, "linkWebsite" character varying NULL, "createAt" TIMESTAMP NOT NULL DEFAULT now(), "updateAt" TIMESTAMP NOT NULL DEFAULT now(), "projectSize" character varying NULL, "status" boolean NULL DEFAULT false, "location" character varying NULL,CONSTRAINT "PK_f57d8293406df4af348402e4b74" PRIMARY KEY ("id"))`,
+            `CREATE TABLE "team" ("id" SERIAL NOT NULL, "teamName" character varying NULL, "teamSize" character varying NULL, "timeZone" character varying NULL, "organization" character varying NULL, "workingTime" character varying NULL, "hour" character varying NULL, "week" character varying NULL, "description" character varying NULL, "avatar" character varying NULL, "avatarUrl" character varying NULL, "slogan" character varying NULL,"founded" character varying NULL, "linkWebsite" character varying NULL, "createAt" TIMESTAMP NOT NULL DEFAULT now(), "updateAt" TIMESTAMP NOT NULL DEFAULT now(), "projectSize" character varying NULL, "status" boolean NULL DEFAULT false, "location" character varying NULL,CONSTRAINT "PK_f57d8293406df4af348402e4b74" PRIMARY KEY ("id"))`,
         );
     }
 
@@ -13,4 +13,3 @@ export class createTeams1653973001413 implements MigrationInterface {
         await queryRunner.query(`DROP TABLE "team"`);
     }
 }
-
