6aec399b72e65b7dcad15130754cea70f3475556
Toan Nguyen <toan.nguyenviet@ncc.asia>
2022-06-09 10:15:56 +0700

    fix lost commit

--- src/portfolio/portfolio.service.ts
+++ src/portfolio/portfolio.service.ts
@@ -1,8 +1,9 @@
+import {User} from '@/user/user.entity';
 import {
-    HttpException,
-    HttpStatus,
-    Injectable,
-    NotFoundException,
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NotFoundException,
 } from '@nestjs/common';
 import {InjectRepository} from '@nestjs/typeorm';
 import {Repository} from 'typeorm';
@@ -12,86 +13,77 @@ import {Portfolio} from './portfolio.entity';
 
 @Injectable()
 export class PortfolioService {
-    constructor(
-        @InjectRepository(Portfolio)
-        private readonly portfolioRepository: Repository<Portfolio>,
-    ) {}
+  constructor(
+    @InjectRepository(Portfolio)
+    private readonly portfolioRepository: Repository<Portfolio>,
+  ) {}
 
-    async createPortfolio(
-        createPortfolioDto: CreatePortfolioDto,
-    ): Promise<Portfolio> {
-        try {
-            const portfolio = new Portfolio();
-            const payload = createPortfolioDto;
-            portfolio.category = payload.category;
-            portfolio.clientWebsite = payload.clientWebsite;
-            portfolio.videoLink = payload.videoLink;
-            portfolio.companyName = payload.companyName;
-            portfolio.content = payload.content;
-            portfolio.description = payload.description;
-            portfolio.imageUrl = payload.imageUrl;
-            portfolio.endDate = payload.endDate;
-            portfolio.startDate = payload.startDate;
-            portfolio.estimate = payload.estimate;
-            portfolio.privacy = payload.privacy;
-            portfolio.title = payload.title;
+  async createPortfolio(
+    createPortfolioDto: CreatePortfolioDto,
+    user: User,
+  ): Promise<Portfolio> {
+    try {
+      const portfolio = new Portfolio({
+        ...createPortfolioDto,
+        team: user as any,
+      });
 
-            return await portfolio.save();
-        } catch (error) {
-            throw new NotFoundException('Error cannot create portfolio');
-        }
+      await portfolio.save();
+      delete portfolio.team;
+      return portfolio;
+    } catch (error) {
+      throw new NotFoundException('Error cannot create portfolio');
     }
+  }
 
-    async updatePortfolio(
-        id: number,
-        updatePortfolioDto: UpdatePortfolioDto,
-    ): Promise<Portfolio> {
-        try {
-            const porfolio = await this.portfolioRepository.findOne(id);
-            if (!porfolio) {
-                throw new NotFoundException(
-                    `There isn't any portfolio with id: ${id}`,
-                );
-            }
+  async updatePortfolio(
+    id: number,
+    updatePortfolioDto: UpdatePortfolioDto,
+  ): Promise<Portfolio> {
+    try {
+      const porfolio = await this.portfolioRepository.findOne(id);
+      if (!porfolio) {
+        throw new NotFoundException(`There isn't any portfolio with id: ${id}`);
+      }
 
-            const updatePortfolio = await this.portfolioRepository.save({
-                id: id,
-                category: updatePortfolioDto.category,
-                clientWebsite: updatePortfolioDto.clientWebsite,
-                companyName: updatePortfolioDto.companyName,
-                content: updatePortfolioDto.content,
-                description: updatePortfolioDto.description,
-                endDate: updatePortfolioDto.endDate,
-                estimate: updatePortfolioDto.estimate,
-                imageUrl: updatePortfolioDto.imageUrl,
-                videoLink: updatePortfolioDto.videoLink,
-                startDate: updatePortfolioDto.startDate,
-                title: updatePortfolioDto.title,
-                privacy: updatePortfolioDto.privacy,
-            });
-            return updatePortfolio;
-        } catch (error) {
-            throw new NotFoundException(`Portfolio with ID ${id} not found`);
-        }
+      const updatePortfolio = await this.portfolioRepository.save({
+        id: id,
+        category: updatePortfolioDto.category,
+        clientWebsite: updatePortfolioDto.clientWebsite,
+        companyName: updatePortfolioDto.companyName,
+        content: updatePortfolioDto.content,
+        description: updatePortfolioDto.description,
+        endDate: updatePortfolioDto.endDate,
+        estimate: updatePortfolioDto.estimate,
+        imageUrl: updatePortfolioDto.imageUrl,
+        videoLink: updatePortfolioDto.videoLink,
+        startDate: updatePortfolioDto.startDate,
+        title: updatePortfolioDto.title,
+        privacy: updatePortfolioDto.privacy,
+      });
+      return updatePortfolio;
+    } catch (error) {
+      throw new NotFoundException(`Portfolio with ID ${id} not found`);
     }
+  }
 
-    async getAllPortfolio(): Promise<Portfolio[]> {
-        return await this.portfolioRepository.find();
-    }
+  async getAllPortfolio(): Promise<Portfolio[]> {
+    return await this.portfolioRepository.find();
+  }
 
-    async getPortfolioById(id: number): Promise<Portfolio[]> {
-        const porfolio = await this.portfolioRepository.find({where: {id: id}});
-        if (!porfolio) {
-            throw new NotFoundException(`Porfolio with ID ${id} not found`);
-        }
-        return porfolio;
+  async getPortfolioById(id: number): Promise<Portfolio[]> {
+    const porfolio = await this.portfolioRepository.find({where: {id: id}});
+    if (!porfolio) {
+      throw new NotFoundException(`Porfolio with ID ${id} not found`);
     }
+    return porfolio;
+  }
 
-    async deletePortfolio(id: number): Promise<void> {
-        const porfolio = await this.portfolioRepository.delete(id);
-        if (porfolio.affected === 0) {
-            throw new NotFoundException(`Portfolio with ID ${id} not found`);
-        }
-        throw new HttpException('Delete portfolio success', HttpStatus.OK);
+  async deletePortfolio(id: number): Promise<void> {
+    const porfolio = await this.portfolioRepository.delete(id);
+    if (porfolio.affected === 0) {
+      throw new NotFoundException(`Portfolio with ID ${id} not found`);
     }
+    throw new HttpException('Delete portfolio success', HttpStatus.OK);
+  }
 }
--- src/team/team.entity.ts
+++ src/team/team.entity.ts
@@ -3,94 +3,91 @@ import {SkillDistribution} from '@/skill-distribution/skill-distribution.entity'
 import {Skill} from '@/skills/skills.entity';
 import {ApiProperty} from '@nestjs/swagger';
 import {
-    BaseEntity,
-    Column,
-    Entity,
-    JoinTable,
-    ManyToMany,
-    ManyToOne,
-    OneToMany,
-    PrimaryGeneratedColumn,
+  BaseEntity,
+  Column,
+  Entity,
+  JoinTable,
+  ManyToMany,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
 } from 'typeorm';
 import {User} from '../user/user.entity';
 
 @Entity()
 export class Team extends BaseEntity {
-    @ApiProperty()
-    @PrimaryGeneratedColumn()
-    id?: number;
+  @ApiProperty()
+  @PrimaryGeneratedColumn()
+  id?: number;
 
-    @Column()
-    teamName?: string;
+  @Column()
+  teamName?: string;
 
-    @Column({nullable: true})
-    teamSize?: string;
+  @Column({nullable: true})
+  teamSize?: string;
 
-    @Column({nullable: true})
-    timeZone?: string;
+  @Column({nullable: true})
+  timeZone?: string;
 
-    @Column({nullable: true})
-    organization?: string;
+  @ManyToMany(() => Skill, (skill) => skill.team)
+  @JoinTable()
+  skills?: Skill[];
 
-    @ManyToMany(() => Skill, (skill) => skill.team)
-    @JoinTable()
-    skills?: Skill[];
+  @Column({nullable: true})
+  workingTime?: string;
 
-    @Column({nullable: true})
-    workingTime?: string;
+  @Column({nullable: true})
+  saleEmail?: string;
 
-    @Column({nullable: true})
-    saleEmail?: string;
+  @Column({nullable: true})
+  description?: string;
 
-    @Column({nullable: true})
-    description?: string;
+  @Column({nullable: true})
+  avatar?: string;
 
-    @Column({nullable: true})
-    avatar?: string;
+  @Column({nullable: true})
+  avatarUrl?: string;
 
-    @Column({nullable: true})
-    avatarUrl?: string;
+  @Column({nullable: true})
+  slogan?: string;
 
-    @Column({nullable: true})
-    slogan?: string;
+  @Column({nullable: true})
+  founded?: string;
 
-    @Column({nullable: true})
-    founded?: string;
+  @Column({nullable: true})
+  linkWebsite?: string;
 
-    @Column({nullable: true})
-    linkWebsite?: string;
+  @Column()
+  createAt?: Date;
 
-    @Column()
-    createAt?: Date;
+  @Column()
+  updateAt?: Date;
 
-    @Column()
-    updateAt?: Date;
+  @Column({nullable: true})
+  projectSize?: string;
 
-    @Column({nullable: true})
-    projectSize?: string;
+  @Column({type: 'boolean', default: false})
+  status?: boolean;
 
-    @Column({type: 'boolean', default: false})
-    status?: boolean;
+  @OneToMany(() => SkillDistribution, (skill) => skill.team, {
+    eager: false,
+  })
+  @JoinTable()
+  skillDistribution?: SkillDistribution[];
 
-    @OneToMany(() => SkillDistribution, (skill) => skill.team, {
-        eager: false,
-    })
-    @JoinTable()
-    skillDistribution?: SkillDistribution[];
+  @OneToMany(() => Portfolio, (portfolio) => portfolio.team, {
+    eager: false,
+  })
+  @JoinTable()
+  portfolio?: Portfolio[];
 
-    @OneToMany(() => Portfolio, (portfolio) => portfolio.team, {
-        eager: false,
-    })
-    @JoinTable()
-    portfolio?: Portfolio[];
+  @ManyToOne((type) => User, (user) => user.team, {eager: false})
+  user?: User;
 
-    @ManyToOne((type) => User, (user) => user.team, {eager: false})
-    user?: User;
-
-    constructor(data: Partial<Team> = {}) {
-        super();
-        this.createAt = this.createAt || new Date();
-        this.updateAt = new Date();
-        Object.assign(this, data);
-    }
+  constructor(data: Partial<Team> = {}) {
+    super();
+    this.createAt = this.createAt || new Date();
+    this.updateAt = new Date();
+    Object.assign(this, data);
+  }
 }
