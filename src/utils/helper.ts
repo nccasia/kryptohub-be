7e8826876f5f937a94a4adff747844ad106d5705
toan.nguyenviet <toan.nguyenviet@ncc.asia>
2022-07-11 09:45:02 +0700

    edit helper

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -29,7 +29,7 @@ export const createQueryBuilder = (
 };
 
 export const imageFileFilter = (_req, file, callback) => {
-  if (!file.originalname.match(/\.(jpg|jpeg|png|gif|webp)$/)) {
+  if (!file.originalname.match(/\.(jpg|jpeg|png|svg|webp)$/)) {
     return callback(new Error('Only image files are allowed!'), false);
   }
   callback(null, true);
