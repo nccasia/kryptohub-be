cc8a7b67932fb41a47bd623e9d3f26e52fe163fd
toan.nguyenviet <toan.nguyenviet@ncc.asia>
2022-08-03 18:00:48 +0700

    update

--- src/skill-distribution/dto/create-skill-distribution.dto.ts
+++ src/skill-distribution/dto/create-skill-distribution.dto.ts
@@ -1,13 +1,13 @@
 import {ApiProperty} from '@nestjs/swagger';
-import {IsNotEmpty} from 'class-validator';
+import {IsNotEmpty, IsOptional} from 'class-validator';
 import {ISkillDistributionValue} from '../skill-distribution.entity';
 
 export class CreateSkillDistributionDto {
-    @ApiProperty({required: true})
-    @IsNotEmpty()
-    readonly skillDistributionName!: string;
+  @ApiProperty({required: true})
+  @IsNotEmpty()
+  readonly skillDistributionName!: string;
 
-    @ApiProperty({required: true})
-    @IsNotEmpty()
-    skillDistributionValue?: ISkillDistributionValue[];
+  @ApiProperty({required: true})
+  @IsOptional()
+  skillDistributionValue?: ISkillDistributionValue[];
 }
